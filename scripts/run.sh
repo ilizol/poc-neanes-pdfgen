01e19805c5d34d8155f14a0d5c07ddd32099df78
Elias Zolotas <ilizol@yahoo.gr>
2025-08-30 13:54:23 +0300

    Update run.sh file.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,3 @@
-npm run start -- ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios_pdfkit.byzx
+npm run start -- ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byzx
 
 mv ~/Projects/poc-neanes-pdfgen/output.pdf ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios_pdfkit.pdf
-
-
-
-npm run start -- ~/Projects/neanes-examples/kontakion_saint_spyridon/kontakion_saint_spyridon_almouzios_pdfkit.byzx
-
-mv ~/Projects/poc-neanes-pdfgen/output.pdf ~/Projects/neanes-examples/kontakion_saint_spyridon/kontakion_saint_spyridon_almouzios_pdfkit.pdf
